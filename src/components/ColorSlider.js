0995bff613d40211614c97eb2903fef1f77a6b75
Julius Sohn <juliussohn@icloud.com>
2020-10-05 11:21:11 +0200

    single steps

--- src/components/ColorSlider.js
+++ src/components/ColorSlider.js
@@ -1,16 +1,16 @@
-import React from "react";
-import chromajs from "chroma-js";
-import Slider, { Range } from "rc-slider";
-import styled from "styled-components";
+import React from 'react'
+import chromajs from 'chroma-js'
+import Slider, { Range } from 'rc-slider'
+import styled from 'styled-components'
 
 const Container = styled.div`
 	margin: 10px 0;
-`;
+`
 const Inputs = styled.div`
 	display: flex;
 	justify-content: space-between;
 	margin-top: 10px;
-`;
+`
 
 const Input = styled.input`
 	display: flex;
@@ -19,37 +19,34 @@ const Input = styled.input`
 	outline: none;
 	border-bottom: 1px solid rgba(0, 0, 0, 0.3);
 	text-align: center;
-`;
+`
 
 const getHueGradient = (lightness, chroma, hue) => {
 	const getSteps = (count = 6) => {
-		let steps = [];
+		let steps = []
 		for (var i = 0; i < count * 2; i++) {
-			const l = lightness[0] + ((lightness[1] - lightness[0]) / count) * i;
-			const c = chroma[0] + ((chroma[1] - chroma[0]) / count) * i;
-			const h = hue[0] + ((hue[1] - hue[0]) / count) * i;
-			steps.push(chromajs.lch(l, c, h));
+			const l = lightness[0] + ((lightness[1] - lightness[0]) / count) * i
+			const c = chroma[0] + ((chroma[1] - chroma[0]) / count) * i
+			const h = hue[0] + ((hue[1] - hue[0]) / count) * i
+			steps.push(chromajs.lch(l, c, h))
 		}
-		return steps;
-	};
+		return steps
+	}
 
-	const stepCount = 12;
+	const stepCount = 12
 
-	const scale = chromajs
-		.scale(getSteps(stepCount))
-		.domain([0, 360])
-		.mode("lch");
-	let steps = scale.classes(stepCount).colors(stepCount);
+	const scale = chromajs.scale(getSteps(stepCount)).domain([0, 360]).mode('lch')
+	let steps = scale.classes(stepCount).colors(stepCount)
 
 	const gradientSteps = steps.map(
 		(step, i) => `${step} ${(100 / stepCount) * i}%`
-	);
+	)
 
-	return `linear-gradient(90deg, ${gradientSteps.join(`,`)})`;
-};
+	return `linear-gradient(90deg, ${gradientSteps.join(`,`)})`
+}
 
 function ColorSlider(props) {
-	const height = 15;
+	const height = 15
 
 	return (
 		<Container>
@@ -70,7 +67,7 @@ function ColorSlider(props) {
 				}}
 				trackStyle={[
 					{
-						backgroundColor: "transparent",
+						backgroundColor: 'transparent',
 						/*height: height + 6,
 						marginTop: -3,
 						border: `3px solid`,
@@ -96,42 +93,39 @@ function ColorSlider(props) {
 				}}
 				pushable={true}
 				allowCross={false}
-				onChange={(value) => props.onChange({ ...props.value, base: value })}
+				onChange={value => props.onChange({ ...props.value, base: value })}
 			/>
 			<Inputs>
 				<Input
-					step="10"
 					type="number"
 					id="hue"
 					name="hue"
 					value={props.value.dark}
-					onChange={(event) =>
+					onChange={event =>
 						props.onChange({ ...props.value, dark: event.target.value })
 					}
 				/>
 				<Input
-					step="10"
 					type="number"
 					id="hue"
 					name="hue"
 					value={props.value.base}
-					onChange={(event) =>
+					onChange={event =>
 						props.onChange({ ...props.value, base: event.target.value })
 					}
 				/>
 				<Input
-					step="10"
 					type="number"
 					id="hue"
 					name="hue"
 					value={props.value.light}
-					onChange={(event) =>
+					onChange={event =>
 						props.onChange({ ...props.value, light: event.target.value })
 					}
 				/>
 			</Inputs>
 		</Container>
-	);
+	)
 }
 
-export default ColorSlider;
+export default ColorSlider
